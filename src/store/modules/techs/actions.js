ac96b847761b8a1a473b601ba5e2655c546d5cf8
Anderson Ganhara <aganhara@gmail.com>
2020-01-14 21:21:05 -0300

    Mockando a api e testando o sagas.
    Usando a lib axios-mock-adapter para mockar o serviço.
    Usando runSaga para chamar um saga no teste.

--- src/store/modules/techs/actions.js
+++ src/store/modules/techs/actions.js
@@ -3,4 +3,17 @@ export function addTech(tech) {
     type: 'ADD_TECH',
     payload: { tech }
   }
+};
+
+export function getTechsSuccess(data) {
+  return {
+    type: 'GET_TECHS_SUCCESS',
+    payload: { data }
+  }
+};
+
+export function getTechsFailure() {
+  return {
+    type: 'GET_TECHS_FAILURE'
+  }
 }
